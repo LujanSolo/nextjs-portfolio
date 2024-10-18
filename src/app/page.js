6fa81b5330ff95444f106e75f061b65f71c9eac1
LujanSolo <lujansolo@proton.me>
2024-10-18 16:23:59 -0700

    initial upload of existing files

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import bg from '../../public/background/home-bg.jpg';
+import lujan from '../../public/background/lujansolo-suit.png';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between relative">
+      <Image src={bg} alt='background image' fill className="w-full h-full object-cover object-center opacity-25" />
+      
+      <div className="w-full h-screen">
+        {/* navigation and 3d model    */}
+      </div>
+    </main>
+  );
+}
