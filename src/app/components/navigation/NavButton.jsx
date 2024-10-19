73bd85cce71b354fb9421bf02175e840843ea1f7
LujanSolo <lujansolo@proton.me>
2024-10-19 13:48:16 -0700

    added switch statement for lucide icons

--- src/app/components/navigation/NavButton.jsx
+++ src/app/components/navigation/NavButton.jsx
@@ -1,26 +1,40 @@
 import React from 'react';
 import Link from 'next/link';
+import { Home, Github, Linkedin, Palette, Phone, Twitter, User, NotebookText } from 'lucide-react';
 
 const getIcon = (icon) => {
-  switch(icon) {
+  switch (icon) {
     case "home":
       return <Home className='w-full h-auto' strokeWidth={1.5} />;
+    case "about":
+      return <User className='w-full h-auto' strokeWidth={1.5} />;
+    case "projects":
+      return <Palette className='w-full h-auto' strokeWidth={1.5} />;
+    case "contact":
+      return <Phone className='w-full h-auto' strokeWidth={1.5} />;
+    case "github":
+      return <Github className='w-full h-auto' strokeWidth={1.5} />;
+    case "linkedin":
+      return <Linkedin className='w-full h-auto' strokeWidth={1.5} />;
+    case "twitter":
+      return <Twitter className='w-full h-auto' strokeWidth={1.5} />;
+    case "resume":
+      return <NotebookText className='w-full h-auto' strokeWidth={1.5} />;
 
-      break;
 
     default:
       return <Home className='w-full h-auto' strokeWidth={1.5} />;
-      
+
   }
 }
 
-const NavButton = ({x, y, label, link, icon, newTab}) => {
+const NavButton = ({ x, y, label, link, icon, newTab }) => {
   return (
     <div className='absolute cursor-pointer z-50'
-      style={{transform: `translate(${x}, ${y})`}}
+      style={{ transform: `translate(${x}, ${y})` }}
     >
       <Link href={link} target={newTab ? '_blank' : '_self'} className='text-foreground rounded-full flex items-center justify-center' aria-label={label} name={label}>
-        <span>{icon}</span>
+        <span className='relative w-16 h-16 p-4'>{getIcon(icon)}</span>
       </Link>
     </div>
   )
