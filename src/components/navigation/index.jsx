42c1c6a7a0c47e2b09eaf0698b9a43f2a8f816d8
LujanSolo <lujansolo@proton.me>
2024-10-25 21:56:25 -0700

    moved navigation buttons inside of responsiveUI component

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -3,33 +3,42 @@ import { BtnList } from "@/app/data";
 import React from "react";
 import NavButton from "./NavButton";
 import useScreenSize from "../hooks/useScreenSize";
-
+import ResponsiveUI from "../ResponsiveUI";
 
 const Navigation = () => {
 
   const angleIncrement = 360 / BtnList.length;
-  const size = useScreenSize(); 
+  const size = useScreenSize();
   const isLarge = size >= 1024;
   const isMedium = size >= 768 && size < 1024;
 
   return (
     <div className="w-full fixed h-screen flex items-center justify-center">
-      <div className="w-max flex items-center justify-center relative hover:pause animate-spin-slow group">
+      <ResponsiveUI>
         {
-          BtnList.map((btn, index) => {
+          ({ size }) => {
+            return size && size >= 480 ? (
+              <div className="w-max flex items-center justify-center relative hover:pause animate-spin-slow group">
+                {
+                  BtnList.map((btn, index) => {
+                    const radianAngle = (index * angleIncrement * Math.PI) / 180;
+                    const radius = isLarge ? 'calc(20vw - 1rem)' : isMedium ? 'calc(30vw - 1rem)' : 'calc(40vw - 1rem)';
+                    const x = `calc(${radius}*${Math.cos(radianAngle)})`;
+                    const y = `calc(${radius}*${Math.sin(radianAngle)})`;
 
-            const radianAngle = (index * angleIncrement * Math.PI) / 180;
-            const radius = isLarge ? 'calc(20vw - 1rem)' : isMedium ? 'calc(30vw - 1rem)' : 'calc(40vw - 1rem)';
-
-            
-            const x = `calc(${radius}*${Math.cos(radianAngle)})`;
-            const y = `calc(${radius}*${Math.sin(radianAngle)})`;
-
-            return <NavButton key={btn.label} x={x} y={y} {...btn} />
-          })
+                    return <NavButton key={btn.label} x={x} y={y} {...btn} />
+                  })
+                }
+              </div>
+            ) : (
+              <h1>responsive navigation</h1>
+            )
+          }
         }
-      </div>
 
+
+
+      </ResponsiveUI>
     </div>
   );
 }
