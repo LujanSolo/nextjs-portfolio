7ea1e523c8c4dc4f5018c86776ef5ffeddcdecb2
LujanSolo <lujansolo@proton.me>
2024-10-28 12:51:37 -0700

    updated apostrophes

--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -12,7 +12,7 @@ export default function Contact() {
             contact the operator
           </h1>
           <p className="text-center font-light text-sm xs:text-base">
-            They`&apos;`ve got you backed into a corner, and you`&apos;`re not sure if there is a way out of it this time. Luckily, you`&apos;`ve got a solid Operator at the end of your fingertips. He`&apos;`s got the programs, data, and components you need to get the webspace job done. Use the form below to transfer your message through the matrix and your operator will respond ASAP... woah.
+            They&apos;ve got you backed into a corner, and you&apos;re not sure if there is a way out of it this time. Luckily, you&apos;ve got a solid Operator at the end of your fingertips. He&apos;s got the programs, data, and components you need to get the webspace job done. Use the form below to transfer your message through the matrix and your operator will respond ASAP... woah.
           </p>
         </div>
         <Form />
