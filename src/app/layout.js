b446126233c6642e600ff004b5c96298d1fe2160
LujanSolo <lujansolo@proton.me>
2024-10-27 23:39:49 -0700

    sound component constructed and added to global layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import clsx from 'clsx';
 import MatrixBg from '../components/MatrixBg';
+import Sound from '@/components/Sound';
+
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -18,6 +20,7 @@ export default function RootLayout({ children }) {
       <body className={clsx(inter.variable, "bg-background text-foreground font-inter")}>
         {children}
         <MatrixBg />
+        <Sound />
       </body>
     </html>
   );
---
+++ src/components/Sound.jsx
@@ -0,0 +1,8 @@
+"use client";
+import React from 'react';
+
+const Sound = () => {
+  return (<div>Sound</div>);
+}
+
+export default Sound;
